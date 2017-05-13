0e10153eb2d44954b289ff2653319a7e0de20790
Tim <timday@Tims-MacBook-Pro.local>
2017-05-13 16:43:49 +0200

    /Users/timday/Documents/workspace/Java2/src/XMLChecker.java

---
+++ XMLChecker.java
@@ -0,0 +1,89 @@
+/**
+ * Course:      Data Structures and Algorithms for Language Processing SS 2017
+ * Assignment:  lab 3, exercise 2. 12.05.2017
+ * Author:      Timothy Day
+ * Description: This program takes a string as an argument and checks it for XML wellformedness
+ *
+ * Honor Code:  I pledge that this program represents my own work.
+ *		I received help from:
+ * 		no one
+ *		in designing and debugging my program.
+ */
+
+import java.util.Stack;
+
+public class XMLChecker {
+	
+	/**
+	 * wellFormedXML checks a String for the XML requirements for wellformedness
+	 * @param xml - a string
+	 * @return true if well formed XML
+	 */
+	public boolean wellformedXML(String xml) {
+		
+		boolean isWellFormed = true;
+		Stack<String> tagStorage = new Stack<String>(); //for storing opening tags
+		String[] line = xml.split(">");
+
+		//check that first token is a valid opening tag. 
+		if (line[0].charAt(0) != '<') {
+			isWellFormed = false;
+		} else if (line[0].charAt(line[0].length()-1) == '/' && line.length > 1) { //if last char is '/' (empty content single tag), and there is more to come
+			isWellFormed = false;
+		}
+		
+		//make sure that first tag matches the last (but only if they are not the same tag!)
+		String rootStart = line[0].substring(1); //trim the leading '<'
+		String rootEnd = line[line.length-1].substring(line[line.length-1].indexOf('<')).substring(2); //trim the leading "</"
+		if (!rootStart.equals(rootEnd) && line.length > 1) { //same name? If not...
+			isWellFormed = false;
+		}
+		
+		if (isWellFormed) { //if already false, save processing
+			String tagPortion, openTag, closeTag = "";
+			
+			for (int i=0; i<line.length; i++) { //go through each token\\
+				
+				//get the tag part of each token, ignoring content
+				tagPortion = line[i].substring(line[i].indexOf('<')); 
+				if (tagPortion.indexOf(' ') > 0){ //if the tag has whitespace in it, it is a bad tag
+					isWellFormed = false;
+				}
+				
+				if (line[i].contains("<")) { //it should, as long as no typos
+					
+					if (tagPortion.indexOf('/') < 0) { 									//opening tag
+						tagStorage.push(tagPortion); //add to stack
+						
+					} else if (tagPortion.charAt(tagPortion.length()-1) == '/') {   	//single empty content tag
+						//do nothing
+					} else if (tagPortion.charAt(1) == '/') {							//closing tag
+						
+						if (!tagStorage.empty()) { 						//check stack. was the previous tag a matching opener?
+							closeTag = tagPortion.substring(2); 		//trim first two chars to get the tag name
+							openTag = tagStorage.pop().substring(1); 	//get previous open tag, trim the leading '<'
+							if (!openTag.equals(closeTag)) { 			//if they don't match, false. XML is case sensitive
+								isWellFormed = false;
+							}
+						} else { 										// no open tag on stack to match the close tag with
+							isWellFormed = false;
+						}
+					}
+					
+				} else { // if no '<' in this token, something was typed incorrectly
+					isWellFormed = false;
+				}
+			} //end for loop
+			
+			if (!tagStorage.empty()) { //should be empty if all the opening tags are accounted for
+				isWellFormed = false;
+			}
+		}
+		return isWellFormed;
+	}
+//	public static void main(String[] args) {
+//		XMLChecker sample = new XMLChecker();
+//		System.out.println(sample.wellformedXML("<a><b>trout<empty/><c>more content here</c></b></a>"));
+//	}
+}
+
---
+++ XMLCheckerTest.java
@@ -0,0 +1,177 @@
+/**
+ * Course:      Data Structures and Algorithms for Language Processing SS 2017
+ * Assignment:  lab 3, exercise 2. 12.05.2017
+ * Author:      Timothy Day
+ * Description: This is a JUnit Test Case class for XMLChecker 
+ *
+ * Honor Code:  I pledge that this program represents my own work.
+ *		I received help from:
+ * 		no one
+ *		in designing and debugging my program.
+ */
+
+import junit.framework.TestCase;
+
+public class XMLCheckerTest extends TestCase {
+
+	String test = "";
+	XMLChecker checker = new XMLChecker();
+	
+	/**
+     * Test for if first token in String is a tag
+     * - no opening <
+     * expect false
+     */
+    public void testBadFirstTag1() {
+    	test = "a> <b> banana </a> </b>";
+        assertFalse(checker.wellformedXML(test));
+    }
+    
+    /**
+     * Test for if first token in String is a tag
+     * - opening tag is of syntax <tag/> (a single empty tag), when there is more content
+     * expect false
+     */
+    public void testBadFirstTag2() {
+    	test = "<a/> <b> banana </a> </b>";
+        assertFalse(checker.wellformedXML(test));
+    }
+    
+    /**
+     * Test an xml doc that opens with a no-content tag that is not the root tag
+     * expect false
+     */
+    public void testBadFirstTag3() {
+    	test = "<empty/><a><b>trout<empty/><c>more content here</c></b></a>";
+        assertFalse(checker.wellformedXML(test));
+    }
+    
+    /**
+     * Test for xml with no white space anywhere
+     * expect true
+     */
+    public void testForNoSpaces() {
+    	test = "<a><b>trout<empty/></b></a>";
+        assertTrue(checker.wellformedXML(test));
+    }
+    
+    /**
+     * Test for xml with white space in content
+     * expect true
+     */
+    public void testWithSpacesInContent() {
+    	test = "<a><b> 	 trout <empty/>  </b></a>";
+        assertTrue(checker.wellformedXML(test));
+    }
+    
+    /**
+     * Test for xml with white space in tags
+     * expect false
+     */
+    public void testWithSpacesInTags() {
+    	test = "<a ><b> 	 trout <empty/>  </b></a>";
+        assertFalse(checker.wellformedXML(test));
+    }
+    
+	/**
+     * Test for when tags are nested incorrectly
+     * expect false
+     */
+    public void testMuddledNesting() {
+    	test = "<a> <b> banana </a> </b>";
+        assertFalse(checker.wellformedXML(test));
+    }
+    
+    /**
+     * Test including single empty content tag
+     * expect true
+     */
+    public void testEmptyContentTag() {
+    	test = "<a><b>trout<empty/><c>more content here</c></b></a>";
+        assertTrue(checker.wellformedXML(test));
+    }
+    
+    /**
+     * Test that all open tags are closed
+     * expect false
+     */
+    public void testTooManyOpeningTags() {
+    	test = "<a><b>trout<empty/><c>more content here</c></b>";
+        assertFalse(checker.wellformedXML(test));
+    }
+    
+    /**
+     * Test a single tag xml doc
+     * expect true
+     */
+    public void testNoContentTagAsRoot() {
+    	test = "<a/>";
+        assertTrue(checker.wellformedXML(test));
+    }
+    
+    
+    /**
+     * Test an xml doc that closes with a tag that is not the root tag
+     * expect true
+     */
+    public void testNotRootAtEnd() {
+    	test = "<a><b>trout<empty/><c>more content here</c></b></a><empty/>";
+        assertFalse(checker.wellformedXML(test));
+    }
+
+    /**
+     * Test for when a '<' was omitted
+     * expect false
+     */
+    public void testTagTypo1() {
+    	test = "<a>b>trout<empty/><c>more content here</c></b></a><empty/>";
+        assertFalse(checker.wellformedXML(test));
+    }
+    
+    /**
+     * Test for when a '>' was omitted
+     * expect false
+     */
+    public void testTagTypo2() {
+    	test = "<a><btrout<empty/><c>more content here</c></b></a><empty/>";
+        assertFalse(checker.wellformedXML(test));
+    }
+    
+    /**
+     * Test for when an extra '>' was added
+     * expect false
+     */
+    public void testTagTypo3() {
+    	test = "<a>><b>trout<empty/><c>more content here</c></b></a><empty/>";
+        assertFalse(checker.wellformedXML(test));
+    }
+    
+    /**
+     * Test for when an extra '<' was added
+     * expect false
+     */
+    public void testTagTypo4() {
+    	test = "<a><<b>trout<empty/><c>more content here</c></b></a><empty/>";
+        assertFalse(checker.wellformedXML(test));
+    }
+    
+    /**
+     * Test for tag with attribute
+     * expect true
+     */
+//    public void testWithAttributes() {
+//    	test = "<a><b att=\"attribute\">trout<empty/><c>more content here</c></b></a>";
+//        assertTrue(checker.wellformedXML(test));
+//    }
+
+    
+    /**
+     * Test for when two tags are opened and closed sequentially
+     * expect false
+     */
+    public void testMoreNesting() {
+    	test = "<a><b>piano</b><c>trumpet</c></a>";
+        assertTrue(checker.wellformedXML(test));
+    }
+}
+
